415669ace04b6e292d2889f46789d4d91de6e7d3
Angelo Luis <Angelo Luis>
2017-02-27 22:39:55 -0300

    incluido o exemplo do EventToCommandBehavior

--- demos/todas-funcionalidades/PrismXamarinForms/PrismXamarinForms/ViewModels/PrismPageAViewModel.cs
+++ demos/todas-funcionalidades/PrismXamarinForms/PrismXamarinForms/ViewModels/PrismPageAViewModel.cs
@@ -1,17 +1,30 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
+using Prism.Navigation;
 using Prism.Services;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace PrismXamarinForms.ViewModels
 {
     public class PrismPageAViewModel : BindableBase
     {
-        public PrismPageAViewModel()
-        {
+        public ObservableCollection<string> Itens { get; set; }
 
+        public DelegateCommand<string> ItemTappedCommand { get; set; }
+
+        public PrismPageAViewModel(INavigationService navigationService)
+        {
+            this.Itens = new ObservableCollection<string>();
+            for (int i = 0; i < 10; i++)
+                this.Itens.Add($"Item {i}");
+
+            this.ItemTappedCommand = new DelegateCommand<string>((linha) =>
+            {
+                navigationService.NavigateAsync($"PrismPageD?texto={linha} selecionado!", useModalNavigation: true);
+            });
         }
     }
 }
